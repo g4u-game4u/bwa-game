b55fa166865ef400e4e514493cdc2de22331c1f8
cesardomingosc4u <cesar.domingos@cidadania4u.com.br>
2026-02-02 17:56:54 -0300

    add: new branch

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   // client_id: 'cidadania4u',
-  client_id: 'cartorioallanguerra',
+  client_id: 'bwa',
   // backend_url_base: 'https://integrador-n8n.grupo4u.com.br/webhook/game4u/taxall',
   backend_url_base: 'http://localhost',
   // backend_url_base: 'https://g4u-mvp-api.onrender.com',
@@ -11,9 +11,9 @@ export const environment = {
   
   // Funifier API Configuration
   funifier_api_url: 'https://service2.funifier.com',
-  funifier_api_key: '68ffd888e179d46fce277c00',
+  funifier_api_key: '690a785ce179d46fce59ed65',
   funifier_base_url: 'https://service2.funifier.com',
-  funifier_basic_token: 'NjhmZmQ4ODhlMTc5ZDQ2ZmNlMjc3YzAwOjY3ZWM0ZTRhMjMyN2Y3NGYzYTJmOTZmNQ==',
+  funifier_basic_token: 'NjkwYTc4NWNlMTc5ZDQ2ZmNlNTllZDY1OjY3ZWM0ZTRhMjMyN2Y3NGYzYTJmOTZmNQ==',
   
   // Cache Configuration
   cacheTimeout: 300000, // 5 minutes in milliseconds
